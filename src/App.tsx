4768c0ef673a4afe355d370a013e80066775fca3
“신형기” <tkaqkeldk@hanatour.com>
2022-01-31 15:08:27 +0900

    todo onchange first

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { GlobalStyle } from "./css";
+import ToDoList from "./TodoList";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
